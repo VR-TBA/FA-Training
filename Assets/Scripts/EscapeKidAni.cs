323657aa8a50d08a79c7d970876e89c8ae03d386
Gordon Pierce <gordonkelliher@gmail.com>
2018-04-02 19:44:03 -0400

    Large set of changes
    Large set of changes including SIB animation across behaviors

---
+++ Assets/Scripts/EscapeKidAni.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeKidAni : MonoBehaviour {
+
+
+	public float speed;
+	Animator anim;
+	Rigidbody kidRigid;
+	private float waitTime;
+	public float startWaitTime;
+
+	public Transform[] moveSpots;
+	private int randomSpot;
+	bool sibBool = false;
+
+	int jumpHash = Animator.StringToHash("Jump");
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		//waitTime = startWaitTime;
+		//randomSpot = Random.Range (0, moveSpots.Length);
+		//transform.LookAt(moveSpots[randomSpot].position);
+	}
+	
+	// Update is called once per frame
+	public void SIB(){
+		sibBool = true;
+		
+	}
+
+	public void stopSIB(){
+		sibBool = false;
+	}
+	void Update () 
+	{
+		if(sibBool == true){
+			anim.Play("Land");
+		}else{
+			anim.SetTrigger("Grounded");
+		}
+		
+	}
+
+	
+
+}
