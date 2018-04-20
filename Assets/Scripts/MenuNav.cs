be16802e6e5601fd9e9aee308472d1f815e85745
Nicholas Wilbur <nickwilbur95@gmail.com>
2018-04-20 18:41:54 -0400

    StartMenu can now be scrolled through using joystick.
    You are now able to scroll through the first menu using the touch controller, although you can only scroll down. Menu cycles back to the top when you hit down at the bottom of the menu.

---
+++ Assets/Scripts/MenuNav.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuNav : MonoBehaviour {
+
+	private Button[] buttons;
+	private int index = 0;
+	private int numButtons;
+	private Button newButton;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		buttons = this.GetComponentsInChildren<Button> ();
+		numButtons = buttons.Length;
+		newButton = buttons [0];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		newButton.Select ();
+
+		if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown))
+		{
+			//If last button in array
+			if (index == numButtons - 1) {
+				newButton = buttons [0];
+				index = 0;
+			} 
+			else {
+				index++;
+				newButton = buttons [index];
+			}
+
+			newButton.OnSelect (null);
+			Debug.Log(newButton.ToString());
+		}			
+	}
+}
